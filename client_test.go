b9c57405e5187a81f0b5bbabf42ca63f0316e031
Dmitrii Limonov <disordervlz@gmail.com>
2024-06-17 19:02:53 +0300

    fix test

--- client_test.go
+++ client_test.go
@@ -1,17 +1,21 @@
 package kanyerest
 
 import (
+	"context"
 	"net/http"
 	"testing"
 	"time"
 )
 
 func TestClient(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancel()
+
 	httpClient := &http.Client{Timeout: 5 * time.Second}
 
 	cl := NewYeRestClient(httpClient)
 
-	got, err := cl.GetQuote()
+	got, err := cl.GetQuote(ctx)
 	if err != nil {
 		t.Errorf("GetQuote() error = %s", err)
 
